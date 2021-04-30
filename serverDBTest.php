b6f17ac96354a8feda712143169326c4f9342151
jacky50737 <jacky50737@gmail.com>
2021-04-30 17:44:48 +0800

    ver 0.4 增加DB測試

--- serverDBTest.php
+++ serverDBTest.php
@@ -9,20 +9,19 @@ $connection = new mysqli($server, $dbuser, $dbpassword, $dbname);
 
 # 檢查連線是否成功
 if ($connection->connect_error) {
-die("連線失敗：" . $connection->connect_error);
+    die("連線失敗：" . $connection->connect_error);
 }
 
 # MySQL/MariaDB 指令
-$sqlQuery = "CREATE TABLE user_table (
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(50),
-age INT UNSIGNED)";
+$sqlQuery = "INSERT INTO DATA (game, no1, no2, no3, no4, no5, no6, no7, no8, no9, no10) VALUES ('99999', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)";
 
 # 執行 MySQL/MariaDB 指令
 if ($connection->query($sqlQuery) === TRUE) {
-echo "成功建立資料表。";
+    # 若有 AUTO_INCREMENT 的 ID 欄位，可直接取得此筆資料的 ID
+    $last_id = $connection->insert_id;
+    echo "成功新增資料，新資料 ID：" . $last_id;
 } else {
-echo "執行失敗：" . $connection->error;
+    echo "執行失敗：" . $connection->error;
 }
 
 # 關閉 MySQL/MariaDB 連線
