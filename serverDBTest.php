d0bca5d13481f99b67578cd56f426f3b2f2c4f88
jacky50737 <jacky50737@gmail.com>
2021-04-30 17:52:07 +0800

    ver 0.4 增加DB測試

--- serverDBTest.php
+++ serverDBTest.php
@@ -26,3 +26,32 @@ if ($connection->query($sqlQuery) === TRUE) {
 
 # 關閉 MySQL/MariaDB 連線
 $connection->close();
+
+# ------------查詢-------------
+
+# 連接 MySQL/MariaDB 資料庫
+$connection = new mysqli($server, $dbuser, $dbpassword, $dbname);
+
+# 檢查連線是否成功
+if ($connection->connect_error) {
+    die("連線失敗：" . $connection->connect_error);
+}
+
+# MySQL/MariaDB 指令
+$sqlQuery = "SELECT * FROM DATA WHERE game = '99999';";
+
+# 執行 MySQL/MariaDB 指令
+if ($result = $connection->query($sqlQuery)) {
+    # 取得結果
+    while ($row = $result->fetch_row()) {
+        printf ("%s：%d\n", $row[0], $row[1]);
+    }
+
+    # 釋放資源
+    $result->close();
+} else {
+    echo "執行失敗：" . $connection->error;
+}
+
+# 關閉 MySQL/MariaDB 連線
+$connection->close();
