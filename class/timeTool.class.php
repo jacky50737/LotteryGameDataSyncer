06de110f3ee9af3b8f02a5da477a32b8250da891
JackyKuo <jacky50737@gmail.com>
2022-01-21 15:35:00 +0800

    Create timeTool.class.php
    時間工具

---
+++ class/timeTool.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Jacky.
+ * Project Name: LotteryGameDataSyncer-main.
+ * Date Time: 2022/1/21 下午 03:26.
+ */
+
+declare(strict_types=1);
+
+class timeTool
+{
+    public function changeTimeType($seconds): string
+    {
+        if ($seconds > 3600){
+            $hours = intval($seconds/3600);
+            $minutes = $seconds % 3600;
+            $time = $hours.":".gmstrftime('%M分%S秒', $minutes);
+        }else{
+            $time = gmstrftime('%H小時%M分%S秒', $seconds);
+        }
+        return $time;
+    }
+}
