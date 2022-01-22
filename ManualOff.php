9a9f4ebd4f7e3a66eb8536e8155e8b9b117d88a1
JackyKuo <jacky50737@gmail.com>
2022-01-22 23:01:15 +0800

    Update ManualOff.php
    重置壽命

--- ManualOff.php
+++ ManualOff.php
@@ -7,7 +7,14 @@
 
 declare(strict_types=1);
 
+require_once('class/DataBaseTool.class.php');
+
+$objDBTool = new DataBaseTool();
+
 $file = fopen("DailyLock.txt", "w");
 fwrite($file, "off");
 fclose($file);
+
+$objDBTool->setLife($fileName, 0);
+
 echo "已解除鎖定";
