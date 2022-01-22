f6d3442a72938883c2729005285c167c65839707
JackyKuo <jacky50737@gmail.com>
2022-01-22 22:59:14 +0800

    Update LotteryDailyData.php
    壽命縮短到700

--- LotteryDailyData.php
+++ LotteryDailyData.php
@@ -26,7 +26,7 @@ try {
     $objDBTool = new DataBaseTool();
     $life = $objDBTool->checkLife($fileName);
 
-    if ($life > 7000) {
+    if ($life > 700) {
         $file = fopen("DailyLock.txt", "w");
         fwrite($file, "off");
         fclose($file);
