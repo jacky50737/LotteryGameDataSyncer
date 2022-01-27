7b2c2099f00dc79a99a9b1257b0cf0074e0cd02c
Jacky <jacky50737@gmail.com>
2022-01-28 03:31:09 +0800

    TEST

--- test.php
+++ test.php
@@ -28,8 +28,21 @@ declare(strict_types=1);
 //var_dump($lastGame);
 //
 
-$start_time = microtime(true);
-$today = date("Y-m-d A h:i:s",intval($start_time + (8 * 60 * 60)));
-var_dump($today);
-echo "done";
+//$start_time = microtime(true);
+//$today = date("Y-m-d A h:i:s",intval($start_time + (8 * 60 * 60)));
+//var_dump($today);
+//echo "done";
+
+$server = "104.168.139.189";         # MySQL/MariaDB 伺服器
+$dbuser = "pjtvqdla_jacky50737";       # 使用者帳號
+$dbpassword = "Aa174677178508123"; # 使用者密碼
+$dbname = "pjtvqdla_PK10";    # 資料庫名稱
+
+# 連接 MySQL/MariaDB 資料庫
+$connection = new mysqli($server, $dbuser, $dbpassword, $dbname);
+
+# 檢查連線是否成功
+if ($connection->connect_error) {
+    die("連線失敗：" . $connection->connect_error);
+}
 
