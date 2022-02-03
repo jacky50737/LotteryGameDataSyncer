36c9b5084f7adb3d1c53100b703619f10b0bf4bf
Jacky <jacky50737@gmail.com>
2022-02-03 21:14:20 +0800

    5秒檢查一次 修正

--- LotteryFastData.php
+++ LotteryFastData.php
@@ -14,22 +14,25 @@ require_once('class/DataBaseTool.class.php');
 $objLineTool = new LineNotify();
 $objLineTool->doLineNotify("\n" . "最新賽車資訊檢查...");
 try {
-    while (date("s") < 58 && (date("s") % 5 == 0)) {
-        $objGameTool = new getPK10GameData();
-        $arrGameData = $objGameTool->getPK10Data("New", "");
+    while (date("s") < 58) {
+        if(intval(date("s")) % 5 == 0){
+            $objGameTool = new getPK10GameData();
+            $arrGameData = $objGameTool->getPK10Data("New", "");
 //var_dump($arrGameData);
-        if ($arrGameData == false) {
-            throw new Exception("取得資料失敗");
-        }
-        $objDBTool = new DataBaseTool();
+            if ($arrGameData == false) {
+                throw new Exception("取得資料失敗");
+            }
+            $objDBTool = new DataBaseTool();
 //var_dump($objDBTool->checkGame(strval($arrGameData[0])));
-        if ($objDBTool->checkGame(strval($arrGameData[0])) == false) {
-            $objDBTool->upLoadGame(strval($arrGameData[0]), $arrGameData[1]);
-            $objLineTool->doLineNotify("\n" . "檢查完畢 新增賽事{$arrGameData[0]}");
-        } else {
-            $objLineTool->doLineNotify("\n" . "檢查完畢 暫無最新賽事");
+            if ($objDBTool->checkGame(strval($arrGameData[0])) == false) {
+                $objDBTool->upLoadGame(strval($arrGameData[0]), $arrGameData[1]);
+                $objLineTool->doLineNotify("\n" . "檢查完畢 新增賽事{$arrGameData[0]}");
+            } else {
+                $objLineTool->doLineNotify("\n" . "檢查完畢 暫無最新賽事");
+            }
+            $objDBTool->closeDB();
         }
-        $objDBTool->closeDB();
+        sleep(1);
     }
 } catch (Exception $exception) {
     if (isset($objDBTool)) {
