b065b85fdcaafba778890a17cc672d6f0c82e294
Jacky <jacky50737@gmail.com>
2022-02-20 23:52:16 +0800

    FIX

--- class/LineNotify.class.php
+++ class/LineNotify.class.php
@@ -24,14 +24,18 @@ class LineNotify
 
         $curl = new CurlTool();
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 4; $i++) {
             $header = array('Authorization:Bearer ' . $this->token[$i]);
-            $results = $curl->doPost($url, $header, $payload);
-            if(!is_null($results->status)){
-                if ($results->message == "ok" || $results->status == 200) {
-                    return true;
-                } 
+            for ($try = 0; $try < 10; $try++) {
+                $results = $curl->doPost($url, $header, $payload);
+                if(!is_null($results->status)){
+                    if ($results->message == "ok" || $results->status == 200) {
+                        $try = 10;
+                        return true;
+                    }
+                }
             }
+
         }
 
         return false;
