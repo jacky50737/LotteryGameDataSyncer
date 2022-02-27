843aa5b3abce20b32f54c5c5322452febb2f46cf
Jacky <jacky50737@gmail.com>
2022-02-27 23:30:35 +0800

    替換辨識方法

--- class/DataBaseTool.class.php
+++ class/DataBaseTool.class.php
@@ -93,7 +93,7 @@ class DataBaseTool
             $sqlQuery = "SELECT game FROM LOG WHERE process = '" . $process . "' AND LAST_DATE = '" . $date . "';";
             for ($i = 0; $i < 5; $i++) {
                 if ($this->connection->query($sqlQuery) == TRUE) {
-                    if(is_null($this->connection->query($sqlQuery)->fetch_assoc()['game'])){
+                    if(empty($this->connection->query($sqlQuery)->fetch_assoc()['game'])){
                         return "無";
                     }
                     return $this->connection->query($sqlQuery)->fetch_assoc()['game'];
