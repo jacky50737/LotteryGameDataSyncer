076ac3eac7e6c56c7774ed5f7618ae592ff82c69
Jacky <jacky50737@gmail.com>
2022-08-16 23:43:46 +0800

    BUG FIX

--- class/DataBaseTool.class.php
+++ class/DataBaseTool.class.php
@@ -169,14 +169,14 @@ class DataBaseTool
      */
     public function updateForecastData($name, $game, $predict, $status)
     {
-        $sqlQuery = "UPDATE forecast SET GAME = '" . $game . "' AND PREDICT = '" . $predict . "' AND STATUS = '" . $status . "' WHERE NAME = '" . $name . "';";
-var_dump($sqlQuery);
+        $sqlQuery = "UPDATE FORECAST SET GAME = '" . $game . "', PREDICT = '" . $predict . "', STATUS = '" . $status . "' WHERE NAME = '" . $name . "';";
+
         for ($i = 0; $i < 5; $i++) {
             if ($this->connection->query($sqlQuery) == TRUE) {
                 return true;
             }
         }
-        var_dump($this->connection->query($sqlQuery));
+
         return false;
     }
 
