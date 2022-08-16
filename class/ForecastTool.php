5f915515e3ae58d33d1481d411ff453f14a4c452
JackyKuo <jacky50737@gmail.com>
2022-08-16 16:52:14 +0800

    Update ForecastTool.php

--- class/ForecastTool.php
+++ class/ForecastTool.php
@@ -20,8 +20,8 @@ class ForecastTool
     }
 
     public function forecastNextGame(string $name, array $gameData){
-        $forecastData = [];
-        $yards = intval(explode('_',$name)[1]);
+        $forecastData = 0;
+//        $yards = intval(explode('_',$name)[1]);
 
         switch ($name){
             case 'YARDS_9_LEVELS_3':
@@ -32,14 +32,6 @@ class ForecastTool
                     $forecastData = $total;
                 }else{
                     $forecastData = $total - 10;
-                }
-
-                for($i=1;$i<=10;$i++){
-                    if($i<=$yards){
-                        $forecastData[] = "{$forecastData}";
-                    }else{
-                        $forecastData[] = "X";
-                    }
                 }
                 break;
             default:
