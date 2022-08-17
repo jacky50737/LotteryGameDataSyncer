d61289f749b102febc75b5868d88f729f12b663c
JackyKuo <jacky50737@gmail.com>
2022-08-17 11:53:15 +0800

    Update test.php

--- test.php
+++ test.php
@@ -64,24 +64,50 @@ $arrGameData[1] = [
     ];
 
 
-$forecastData = $objDBTool->getForecastData();
-//var_dump($forecastData);
-foreach ($forecastData as $row){
-    $status = $forecastTool->checkForecastStatus($arrGameData[1], $row['predict'], $row['name']);
-    $status_C = "初始化";
-    $forecastResult = $forecastTool->processeForecastStatus($row['status'], $status);
-    $row['status'] = $forecastResult['status'];
-//    var_dump($row['c_name']."-本期預測結果：".$forecastResult['result']);
-    if(in_array($row['status'],['SHOOT','DOWN'])){
-        $pass2Data = $objDBTool->getGameData(intval($arrGameData[0]-2)); //抓-2期資料
-        $gameData = $pass2Data['game'];
-        unset($pass2Data['game']);
-//        var_dump($gameData);
-//        var_dump($pass2Data);
-        $getPredict = $forecastTool->forecastNextGame($row['name'],$pass2Data);
-        $objDBTool->updateForecastData($row['name'],$arrGameData[1],$row['predict'],$row['status']);
-    }
-}
+//$forecastData = $objDBTool->getForecastData();
+////var_dump($forecastData);
+//foreach ($forecastData as $row){
+//    $status = $forecastTool->checkForecastStatus($arrGameData[1], $row['predict'], $row['name']);
+//    $status_C = "初始化";
+//    $forecastResult = $forecastTool->processeForecastStatus($row['status'], $status);
+//    $row['status'] = $forecastResult['status'];
+////    var_dump($row['c_name']."-本期預測結果：".$forecastResult['result']);
+//    if(in_array($row['status'],['SHOOT','DOWN'])){
+//        $pass2Data = $objDBTool->getGameData(intval($arrGameData[0]-2)); //抓-2期資料
+//        $gameData = $pass2Data['game'];
+//        unset($pass2Data['game']);
+////        var_dump($gameData);
+////        var_dump($pass2Data);
+//        $getPredict = $forecastTool->forecastNextGame($row['name'],$pass2Data);
+//        $objDBTool->updateForecastData($row['name'],$arrGameData[1],$row['predict'],$row['status']);
+//    }
+//}
 
 //$getPredict = $forecastTool->forecastNextGame();
 
+//----KNN TEST-----
+
+//取得主機使用狀況
+function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
+
+function get_server_cpu_usage(){
+
+    $load = sys_getloadavg();
+    return $load[0];
+
+}
+echo "RAM：";
+print get_server_memory_usage();
+echo "\nCPU";
+print get_server_cpu_usage();
