de964ac388477efb0460e2d486cf78277a2624c4
JackyKuo <jacky50737@gmail.com>
2022-08-17 17:01:58 +0800

    Update ForecastTool.php

--- class/ForecastTool.php
+++ class/ForecastTool.php
@@ -34,6 +34,7 @@ class ForecastTool
             case 'YARDS_8_LEVELS_3':
             case 'YARDS_5_LEVELS_3':
             case 'YARDS_9_LEVELS_2':
+            case 'YARDS_1_LEVELS_10':
                 $total = intval($gameData['no1']) + intval($gameData['no10']);
 
                 if ($total <= 10) {
