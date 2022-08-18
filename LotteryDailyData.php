a2a23ded948dfd97ea192ad9295bf5c98dc8520d
JackyKuo <jacky50737@gmail.com>
2022-08-18 11:34:11 +0800

    加速

--- LotteryDailyData.php
+++ LotteryDailyData.php
@@ -143,7 +143,7 @@ try {
 //                            "\n" . "本期[" . $game . "]已存在，前往下一期賽事" .
 //                            "\n" . "還有[" . ($total - $done) . "]筆賽事，");
                     }
-                    usleep(10000);
+                    usleep(1000000);
                     if($done%50 == 0){
                         $now_time = microtime(true);
                         $cost_time = $now_time - $start_time;
